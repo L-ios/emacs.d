2f0ff9d175cf211c40185621e7800eb589d954ad
Lionseun Ling <lionseun@gmail.com>
2023-04-09 23:47:28 +0800

    delete preload

--- lisp/init-preload-local.el
+++
@@ -1,9 +0,0 @@
-;;; init-preload-local.el --- preload local -*- lexical-binding: t -*-
-;;; Commentary:
-;;; Code:
-
-(setq org-default-notes-file "~/.emacs.d/note/index.org")
-
-(provide 'init-preload-local)
-
-;;; init-preload-local.el ends here
